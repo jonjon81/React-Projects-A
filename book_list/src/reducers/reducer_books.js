e47dbd9788cfd8f053480294f282fec8bbd1e152
Jon Atlas <jjjatlas@hotmail.com>
2017-03-29 08:00:49 -0400

    first commit for weather app

--- book_list/src/reducers/reducer_books.js
+++ book_list/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        {title: 'Javascript: The Good Parts'},
-        {title: 'Harry Potter' },
-        {title: 'Superman'},
-        {title: 'Batman Destroys' },
+        { title: 'Javascript: The Good Parts', pages: 342 },
+        { title: 'Harry Potter', pages: 98 },
+        { title: 'Superman', pages: 34 },
+        { title: 'Batman Destroys ', pages: 2 },
     ]
 }
